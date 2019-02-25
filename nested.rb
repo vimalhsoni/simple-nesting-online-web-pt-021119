8f73b5d719cc710541414964f12b4c8223e585a8
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 00:30:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -67,24 +67,29 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-	programmer_hash =
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
-		 programmer_hash[:yukihiro_matsumoto] => {:known_for => "Ruby",:languages => ["LISP", "C"]}
-	 end
-		 programmer_hash
+programmer_hash =
+{
+		:grace_hopper => {
+			:known_for => "COBOL",
+			:languages => ["COBOL", "FORTRAN"]
+		},
+		:alan_kay => {
+			:known_for => "Object Orientation",
+			:languages => ["Smalltalk", "LISP"]
+		},
+		:dennis_ritchie => {
+			:known_for => "Unix",
+			:languages => ["C"]
+		}
+ }
+
+programmer_hash[:yukihiro_matsumoto] = {
+known_for: "Ruby",
+languages:
+	["LISP", "C"]
+}
+
+programmer_hash
 end
 
 def changing_alan
