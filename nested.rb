49c44bc742b985555c4a20d0d046ee0e93a477a0
Vimal Soni <vimal.h.soni@gmail.com>
2019-02-25 00:37:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -103,7 +103,7 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+     #alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = "GUI"
 		 programmer_hash
 
@@ -125,5 +125,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash 
 end
